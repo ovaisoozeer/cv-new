eed68829d2cf4c41781c2bd96e697ce1da8c8326
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2023-05-31 17:11:41 +0100

    Styled header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,16 +4,26 @@
 
 <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 
-<header>Portfolio site</header>
-<nav>
-	<a href="/">Home</a>
-	<a href="/status">Status</a>
-	<a href="/plan">Plan</a>
-	<a href="/projects">Projects</a>
-	<a href="/skills">Skills</a>
-	<a href="/employment">Employment</a>
-</nav>
-<slot />
+<div class="container mx-auto">
+	<div class="flex flex-row pt-8">
+		<div class="basis-1/2">
+			<header class="w-max text-5xl">Portfolio site</header>
+		</div>
+		<div class="basis-1/2 text-justify text-xl">
+			<nav>
+				<a href="/" class="pl-4">Home</a>
+				<a href="/status" class="pl-4">Status</a>
+				<a href="/plan" class="pl-4">Plan</a>
+				<a href="/projects" class="pl-4">Projects</a>
+				<a href="/skills" class="pl-4">Skills</a>
+				<a href="/employment" class="pl-4">Employment</a>
+			</nav>
+		</div>
+	</div>
+	<div class="pt-8">
+		<slot />
+	</div>
+</div>
 
 <style lang="postcss">
 	:global(html) {
