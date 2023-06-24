b27923380cb9aaaa1ecb819f4a59cf123813e8c2
Ovais Oozeer <ovais.oozeer@users.noreply.github.com>
2023-06-24 15:23:16 +0100

    Adds basic notion SDK client

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,18 @@
 import posts from '../data';
+import {Client} from '@notionhq/client';
+import { env } from '$env/dynamic/private';
 
-export function load() {
+export async function load() {
+	const NOTION_TOKEN = env.NOTION_TOKEN;
+	const notion = new Client({
+	    auth: NOTION_TOKEN,
+	});
+
+	const response =
+		await notion.users.list({});
+
+	console.log("Got response:", response);
+
 	return {
 		testdata: posts.map((post) => ({
 			slug: post.slug,
