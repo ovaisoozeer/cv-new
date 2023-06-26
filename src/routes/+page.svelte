d13effa05079b0384a87891e84b5f3781c325905
Ovais Oozeer <ovais.oozeer@users.noreply.github.com>
2023-06-26 18:08:19 +0100

    extracts content to component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,7 @@
 <script>
+	import DynamicPage from './DynamicPage.svelte';
+
 	export let data;
 </script>
 
-<h1 class="text-3xl font-bold underline">{data.title}</h1>
-
-{#each data.blocks as block}
-	{#if block.blockType === 'h1'}
-		<h2 class="text-3xl">{block.text}</h2>
-	{:else if block.blockType === 'h2'}
-		<h2 class="text-2xl">{block.text}</h2>
-	{:else if block.blockType === 'p'}
-		<p class="text-base">{block.text}</p>
-	{/if}
-{/each}
+<DynamicPage {data} />
