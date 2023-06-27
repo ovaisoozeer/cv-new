c56240e472d25ae7687c2b041a237e3e962a6d5c
Ovais Oozeer <ovais.oozeer@users.noreply.github.com>
2023-06-27 13:01:02 +0100

    adds burger nav and test-data page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,22 +5,79 @@
 <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 
 <div class="container mx-auto">
-	<div class="flex flex-row pt-8">
-		<div class="basis-1/2">
-			<header class="w-max text-5xl">Portfolio site</header>
+	<nav class="bg-white border-gray-200 dark:bg-gray-900">
+		<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+			<a href="/" class="flex items-center">
+				<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+					>Ovais Oozeer</span
+				>
+			</a>
+			<button
+				data-collapse-toggle="navbar-default"
+				type="button"
+				class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+				aria-controls="navbar-default"
+				aria-expanded="false"
+			>
+				<span class="sr-only">Open main menu</span>
+				<svg
+					class="w-6 h-6"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clip-rule="evenodd"
+					/></svg
+				>
+			</button>
+			<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+				<ul
+					class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+				>
+					<li>
+						<a
+							href="/current"
+							class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+							aria-current="page">Profile</a
+						>
+					</li>
+					<li>
+						<a
+							href="/seeking"
+							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+							>Seeking</a
+						>
+					</li>
+					<li>
+						<a
+							href="/skills"
+							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+							>Skills</a
+						>
+					</li>
+					<li>
+						<a
+							href="/employment"
+							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+							>Employment</a
+						>
+					</li>
+					<li>
+						<a
+							href="/Contact"
+							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+							>Contact</a
+						>
+					</li>
+				</ul>
+			</div>
 		</div>
-		<div class="basis-1/2 text-justify text-xl">
-			<nav>
-				<a href="/" class="pl-4">Home</a>
-				<a href="/now" class="pl-4">Now</a>
-				<a href="/next" class="pl-4">Next</a>
-				<a href="/education" class="pl-4">Education</a>
-				<a href="/skills" class="pl-4">Skills</a>
-				<a href="/employment" class="pl-4">Employment</a>
-			</nav>
-		</div>
-	</div>
-	<div class="pt-8">
+	</nav>
+
+	<div>
 		<slot />
 	</div>
 </div>
--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -3,6 +3,31 @@ import { capitalizeFirstLetter } from '$lib/string_utilities';
 
 export async function load({ params }) {
 	let pageTitle = params.slug;
+
+	if (pageTitle === 'test-data') {
+		return {
+			title: 'test data',
+			blocks: [
+				{
+					blockType: 'h1',
+					text: 'Title'
+				},
+				{
+					blockType: 'h2',
+					text: 'Header'
+				},
+				{
+					blockType: 'h3',
+					text: 'Sub title'
+				},
+				{
+					blockType: 'p',
+					text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+				}
+			]
+		};
+	}
+
 	let blockElements = await getPageData(pageTitle);
 
 	return {
