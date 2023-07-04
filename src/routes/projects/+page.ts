d61e26ac962159b84e352665440e0791b25a13eb
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2023-07-04 15:25:56 +0100

    minor refactor

--- src/routes/projects/+page.ts
+++ src/routes/projects/+page.ts
@@ -3,7 +3,7 @@ import { getProjectRows } from '$lib/page_data_helpers';
 export const ssr = false;
 
 export async function load() {
-	var projectRows = await getProjectRows('0e8541767ed14a42a56730e7541ee028');
+	const projectRows = await getProjectRows('0e8541767ed14a42a56730e7541ee028');
 	const data = structuredClone(projectRows.reverse());
 	console.log(data);
 	return {
