125c73e816a07bd01602534abee975b20840c31e
Ovais Oozeer <ovais.oozeer@users.noreply.github.com>
2023-07-06 11:58:53 +0100

    minor: updates param name

--- src/lib/api-helpers.ts
+++ src/lib/api-helpers.ts
@@ -1,8 +1,8 @@
 import type { BlockElement } from './display_types/block_element';
 import type { ProjectRow } from './display_types/project_row';
 
-export async function getPageBlocks(pageId: string): Promise<Array<BlockElement>> {
-	const resp = await fetch(`/api/page-elements?pageId=${pageId}`);
+export async function getPageBlocks(title: string): Promise<Array<BlockElement>> {
+	const resp = await fetch(`/api/page-elements?title=${title}`);
 	const blockElements = await resp.json();
 	return blockElements;
 }
--- src/routes/api/page-elements/+server.ts
+++ src/routes/api/page-elements/+server.ts
@@ -6,7 +6,7 @@ const MAX_AGE = env.MAX_AGE;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET({ url, setHeaders, request }) {
-	const pageTitle = url.searchParams.get('pageId') || '';
+	const pageTitle = url.searchParams.get('title') || '';
 
 	setHeaders({
 		'cache-control': 'max-age=' + (MAX_AGE || 300)
